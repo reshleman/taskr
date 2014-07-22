7f2086b181a59a7c290cdf6b60afe9e2026ee44c
Nicholas Narh <nicknarh@gmail.com>
2014-07-22 14:01:38 -0400

    Create Dashboards show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+require "monban/constraints/signed_in"
+
 Rails.application.routes.draw do
+  constraints Monban::Constraints::SignedIn.new do 
+    root "dashboards#show", as: :dashboard
+  end
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   root "homes#show"
